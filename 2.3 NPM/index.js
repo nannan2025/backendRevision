d6f77810778a1957c7b22f4593dddeca3d03d9ea
Nan Lin Wah Sin Htoo (Nan Nan) <121724887+nannan2025@users.noreply.github.com>
2024-01-22 21:13:21 -0500

    NPMModules
    Testing NPM Modules

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,9 @@
+import generateName from "sillyname";
+var sillyName = generateName();
+
+console.log("My nick name is " + sillyName + ".");
+
+import superheroes from "superheroes";
+var superheroName = superheroes.random();
+
+console.log("My superhero name is " + superheroName + ".");
